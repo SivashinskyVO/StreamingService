3fd39ffedc998d4d3d94facd997786b8f917b754
Vadim Sivashinskiy <vsivashinskiy@modem.by>
2022-10-19 22:00:39 +0300

    Add configuration of CORS and IISIntegration.

--- API/Program.cs
+++ API/Program.cs
@@ -1,32 +1,34 @@
+using API.Helpers;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var services = builder.Services;
+
+services.ConfigureCors();
+services.ConfigureIISIntegration();
+
+services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
 
-app.MapGet("/weatherforecast", () =>
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
+    ForwardedHeaders = ForwardedHeaders.All
 });
 
-app.Run();
+app.UseCors("CorsPolicy");
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
